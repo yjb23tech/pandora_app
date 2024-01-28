e53800165f0e3946a143775b179a212d94f2d3ee
yjb23tech <yjb23tech@gmail.com>
2024-01-28 00:06:16 +0000

    Yeah

--- App.jsx
+++ App.jsx
@@ -4,6 +4,15 @@ const App = () => {
     return (
         <div>
             <h1>Sanity Check</h1>
+            <Card 
+            
+                song_card_img = "track_1_song_art.png"
+                song_card_song_title = "Blame Game"
+                song_card_album_name = "My Beautiful Dark Twisted Fantasy"
+                song_card_artist_name = "Kanye West"
+                song_card_genre_name = "Baroquq Sophisticated Ignorance"
+
+            />
         </div>
     )
 }
